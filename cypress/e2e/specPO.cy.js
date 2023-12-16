f3bf0d1053bb20e661bb44b2174077a309e71c81
Francisco_Esteban <rept2000@gmail.com>
2023-12-16 02:55:45 +0100

    Actualizacion .....

--- cypress/e2e/specPO.cy.js
+++ cypress/e2e/specPO.cy.js
@@ -1,6 +1,24 @@
 /// <reference types="cypress" />
 
-import { MainPage } from "../page-objects/main-page"; 
-import { LoginPage } from "../page-objects/login-page"; 
-import { SecureAreaPage } from "../page-objects/secure-page";
+import { MainPage } from "../page_objects/main-page";
+import { LoginPage } from "../page_objects/login-page";
+import { SecureAreaPage } from "../page_objects/secure-page";
 
+describe("pruebas.login", () => {
+
+const MainPage = new MainPage();
+const LoginPage = new LoginPage();
+const SecureAreaPage = new SecureAreaPage();
+
+it("The user is logged correctly", () => {
+    mainPage.visitMainPage();
+    mainPage.clickFormAuthentication();
+    loginPage.enterUser("tomsmith");
+    loginPage.enterPass("Supersecretpassword!");
+    loginPage.clickLoginButton();
+    SecureAreaPage.checkMessage("You are logged into a Secure Area");
+});
+
+
+
+})
