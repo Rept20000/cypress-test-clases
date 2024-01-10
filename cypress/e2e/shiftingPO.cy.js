a5c754dcd2443c2233267d8b5fcfc5985e17ba7e
Francisco_Esteban <rept2000@gmail.com>
2024-01-10 01:27:06 +0100

    Shift Content Test

--- cypress/e2e/shiftingPO.cy.js
+++ cypress/e2e/shiftingPO.cy.js
@@ -1,42 +1,29 @@
 /// <reference types="cypress" />
 // Segundo test de Cypress/Shifting Content
 
-//import { MainPage } from "../page_objects/main-page";
-//import { ShiftingContent } from "../page_objects/shiftingContent";
-
-//describe("Shifting Content Test", () => {
-
-  //  const mainPage = new M();
-    //const ShiftingContent = new ShiftingContent();
-    
-//it("should verify the table contains 5 elements", () => {
-  //  mainPage.visitMainPage();
-    //mainPage.clickshiftingContent();
-
-// Verify the table contains 5 elements usando get
-
-//cy.get('table').should('have.length', 1).find('tr').should('have.length', 5);
-    
-//});
-
-//})
-
 import { MainPage } from "../page_objects/main-page";
 import { ShiftingContent } from "../page_objects/shiftingContent";
 
 describe("Shifting Content Test", () => {
+
   const mainPage = new MainPage();
   const shiftingContent = new ShiftingContent();
+    
+it("should verify the table contains 5 elements", () => {
+  mainPage
+  .visitMainPage();
+  mainPage
+  .clickshiftingContent();
 
-  it("should verify the table contains 5 elements", () => {
-    mainPage
-      .visitMainPage()
-      .clickShiftingContent();
+  shiftingContent.navigateToMenuElement();
 
-    shiftingContent.navigateToMenuElement();
+// Verify the table contains 5 elements usando get
 
-    // Verify the table contains 5 elements usando get
-    cy.get('table').should('have.length', 1).find('tr').should('have.length', 5);
-  });
+cy.get('table').should('have.length', 1).find('tr').should('have.length', 5);
+    
 });
+
+})
+
+
 
