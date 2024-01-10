36b57462b976a50468e11846f3104dd54f8afba6
Francisco_Esteban <rept2000@gmail.com>
2024-01-10 01:00:55 +0100

    Shift Content Test

--- cypress/e2e/shiftingPO.cy.js
+++ cypress/e2e/shiftingPO.cy.js
@@ -1,17 +1,17 @@
 /// <reference types="cypress" />
 // Segundo test de Cypress/Shifting Content
 
-import { MainPage2 } from "../page_objects/main-page2";
+import { MainPage } from "../page_objects/main-page";
 import { ShiftingContent } from "../page_objects/shiftingContent";
 
 describe("Shifting Content Test", () => {
 
-    const MainPage2 = new MainPage2();
+    const mainPage = new MainPage();
     const ShiftingContent = new ShiftingContent();
     
 it("should verify the table contains 5 elements", () => {
-    MainPage2.visitMainPage2();
-    MainPage2.clickshiftingContent();
+    mainPage.visitMainPage();
+    mainPage.clickshiftingContent();
 
 // Verify the table contains 5 elements usando get
 
