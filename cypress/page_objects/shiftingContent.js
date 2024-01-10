3d07fa5acfad86bbc0f742990e5541c9d09f3fc1
Francisco_Esteban <rept2000@gmail.com>
2024-01-10 01:31:53 +0100

    Shift Content Test

--- cypress/page_objects/shiftingContent.js
+++ cypress/page_objects/shiftingContent.js
@@ -1,24 +1,14 @@
 /// <reference types="cypress" />
 
-//export class ShiftingContent {
-
-  //  visitshiftingContent() 
-    
-    //{
-    //navigateToMenuElement() 
-    //cy.get(':nth-child(39) > a').click();
-    //}
-
-//}
-
-
 export class ShiftingContent {
 
-    visitshiftingContent() {
-      cy.visit('https://the-internet.herokuapp.com/shifting_content');
+    visitshiftingContent() 
+    
+    {
+
+    navigateToMenuElement() 
+    
+    cy.get(':nth-child(39) > a').click();
     }
-  
-    navigateToMenuElement() {
-      cy.get(':nth-child(39) > a').click();
-    }
-  }
+
+}
